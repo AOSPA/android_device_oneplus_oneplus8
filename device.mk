4f8d66b9681ec36215a995c1528b8f6e3ea693f4
Alexander Koskovich <zvnexus@outlook.com>
2020-06-01 12:42:02 +0000

    op8: Import Bluetooth overlays from LA.UM.8.12.r1-13100-sm8250.0.
    Change-Id: I4d7eeba8a80d68e75217d900f8f9d8fb3ba649ba

--- device.mk
+++ device.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     device/oneplus/oneplus8
 
+# Overlays
+PRODUCT_PACKAGES += \
+    OnePlus8SeriesBluetooth
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
