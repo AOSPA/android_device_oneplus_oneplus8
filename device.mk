22076afe212c05ad473b7403f54a5a3e3fc95013
Alexander Koskovich <zvnexus@outlook.com>
2021-04-14 18:56:34 -0700

    op8: init: Cleanup permissions.
     * Only grant permissions to nodes if userspace wants
       access. Originally I just took everything from stock
       but we can clean up quite a bit of this.
    
    Change-Id: I4416fe04c04e6b065c15cd5dd3e305f41570bcbe

--- device.mk
+++ device.mk
@@ -190,8 +190,6 @@ PRODUCT_PACKAGES += \
     init.oneplus.fingerprint.rc \
     init.oneplus.haptics.rc \
     init.oneplus.power.rc \
-    init.oneplus.thermal.rc \
-    init.oneplus.ufs.rc \
     init.oneplus.usb.rc \
     init.qti.chg_policy.sh \
     init.qti.dcvs.sh \
