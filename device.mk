0aa83213353cd4cfe0d29cd1a8ea4a660246790c
Vishalcj17 <vishalcj@aospa.co>
2021-10-28 16:03:48 +0900

    op8: Update Fingerprint HAL to 2.3 with UDFPS support
    Change-Id: I18f9418bebf0d9bc02b20bed1a14487103cd1f98

--- device.mk
+++ device.mk
@@ -54,19 +54,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Biometrics
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    vendor/pa/config/permissions/vendor.aospa.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/vendor.aospa.biometrics.fingerprint.inscreen.xml
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.2-service.oneplus8 \
-    vendor.aospa.biometrics.fingerprint.inscreen@1.0-service \
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus8 \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
-    vendor.oneplus.fingerprint.extension@1.0.vendor
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    persist.sys.fod.pos.x=453 \
-    persist.sys.fod.pos.y=1823 \
-    persist.sys.fod.size=174
+    vendor.oneplus.fingerprint.extension@1.0.vendor \
+    vendor.oneplus.hardware.display@1.0.vendor
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.boot.fingerprintbstate=orange
